05e1dc60d366df046d708747d825ea7c5c327613
vafaeo_ar <vafaei_ar@yahoo.com>
2022-12-13 19:13:51 +0100

    debug

--- ahunt/backend.py
+++ ahunt/backend.py
@@ -362,7 +362,9 @@ def analysis(session_state):
         ground_truth = df_gt['label'].values
         fig = confusion_matrix_figure(predictions, ground_truth)
         
-        human_label = df_gt['human_label'].values
+        df_gtp = df_gt.dropna()
+        human_label = df_gtp['human_label'].values
+        ground_truth = df_gtp['label'].values
         fig = confusion_matrix_figure(human_label, ground_truth, ylabel = 'human labels')
         
         st.pyplot(fig)
